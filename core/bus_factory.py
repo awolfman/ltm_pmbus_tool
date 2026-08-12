b341d548ec014b1be8145a2861ac816224e64324
awolfman <35307733+awolfman@users.noreply.github.com>
2026-08-12 21:37:04 +0300

    Update bus_factory.py
    Изменения по результатам работы с CH341T

--- core/bus_factory.py
+++ core/bus_factory.py
@@ -1,21 +1,32 @@
-"""Bus factory -- unified bus creation.
-
-    bus_num   0.. 99  -> smbus2.SMBus        /dev/i2c-N
-    bus_num 100..199  -> CH341Bus            USB CH341 #N
-    bus_num 200..299  -> FtdiBus             USB FTDI  #N
+# core/bus_factory.py
+"""
+Bus factory - supports CH341 and FTDI adapters.
 """
 
-import smbus2
+import threading
+from .ch341_i2c import CH341Bus
+#from .ftdi_i2c import FTDIBus   # пока закомментировано
 
-CH341_OFFSET = 100
-FTDI_OFFSET  = 200
+# Offsets for bus numbering (используются в GUI)
+CH341_OFFSET = 0
+FTDI_OFFSET = 100
 
+# Глобальный кэш для хранения открытых адаптеров
+_BUS_CACHE = {}
+_CACHE_LOCK = threading.Lock()
 
 def create_bus(bus_num):
-    if bus_num >= FTDI_OFFSET:
-        from core.ftdi_i2c import FtdiBus
-        return FtdiBus(bus_num - FTDI_OFFSET)
-    if bus_num >= CH341_OFFSET:
-        from core.ch341_i2c import CH341Bus
-        return CH341Bus(bus_num - CH341_OFFSET)
-    return smbus2.SMBus(bus_num)
+    global _BUS_CACHE
+
+    with _CACHE_LOCK:
+        if bus_num < FTDI_OFFSET:
+            if bus_num not in _BUS_CACHE:
+                # Физически инициализируем USB-адаптер только ОДИН раз
+                _BUS_CACHE[bus_num] = CH341Bus(bus_num)
+            return _BUS_CACHE[bus_num]
+        else:
+            # TODO: реализовать FTDI
+            raise NotImplementedError("FTDI support not yet implemented")
+
+def create_bus_from_index(bus_num):
+    return create_bus(bus_num)
